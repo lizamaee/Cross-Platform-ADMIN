22e5cbc02d409d446d6535abf3345b24cebb13e4
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-17 20:12:30 +0800

    add state management zustand

---
+++ src/state.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand"
+
+type AuthState = {
+    isAuthenticated: boolean;
+    token: string | null;
+    loginAdmin: (token: string) => void;
+    logoutAdmin: () => void;
+}
+
+  
+export const useAuthStore = create<AuthState>((set) => ({
+    isAuthenticated: false,
+    token: null,
+  
+    //Login
+    loginAdmin: (token: string) => {
+      localStorage.setItem('adminToken', token);
+      set({ isAuthenticated: true, token });
+    },
+    
+    //Logout
+    logoutAdmin: () => {
+      localStorage.removeItem('token');
+      set({ isAuthenticated: false, token: null });
+    }
+  }));
