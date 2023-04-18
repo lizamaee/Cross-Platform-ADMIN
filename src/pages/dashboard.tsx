860f396cbdcc49f5c81ae471c9b667e5ad34b2e8
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-18 21:50:31 +0800

    add navbar style (desktop)

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { RxCross2,RxHamburgerMenu } from "react-icons/rx";
+import { BiHomeAlt } from "react-icons/bi";
+import { MdHowToVote } from "react-icons/md";
+import { HiOutlineLogout } from "react-icons/hi";
 import { Outlet, NavLink } from "react-router-dom";
 
 export default function Dashboard() {
@@ -9,16 +12,16 @@ export default function Dashboard() {
     setIsSidebarOpen(!isSidebarOpen);
   };
   return (
-    <div className="container min-h-screen max-w-screen-2xl flex bg-white">
+    <div className="container min-h-screen max-w-screen-2xl flex bg-[#F0F0F0]">
       <nav
-        className={` sidebar bg-[#262A56] shadow-2xl ${
+        className={` sidebar bg-white text-black shadow-2xl ${
           isSidebarOpen ? 'translate-x-0 flex-[6]' : '-translate-x-full'
         } transition-all duration-300 ease-in-out md:translate-x-0 md:flex-[2] lg:flex-1`}
       >
 
         {/* MOBILE AND TABLET VIEW */}
         { isSidebarOpen && (
-          <ul className="lg:hidden text-white">
+          <ul className="md:hidden">
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
@@ -26,11 +29,26 @@ export default function Dashboard() {
         )}
 
         {/* DESKTOP VIEW */}
-        <ul className="hidden lg:block text-white">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-        </ul>
+        <div className="hidden md:block min-h-screen">
+
+          <div className="flex items-center gap-2 px-5 py-4 shadow-sm">
+            <MdHowToVote size={40} className="text-[#7268EF]"/>
+            <h3 className="text-2xl pop-bold text-[#2A2F4F]">VS Admin</h3>
+          </div>
+
+          <ul className="px-3 pt-10 pop-medium tracking-wider">
+            <li className="flex items-center gap-3 bg-gradient-to-r from-[#7268EF] to-[#9D93F6] text-white px-5 py-3 rounded-md">
+              <BiHomeAlt size={25}/>
+              <NavLink to="/">Dashboard</NavLink>
+            </li>
+          </ul>
+          
+          <div className="logout w-full text-lg flex justify-between items-center pop-semibold absolute p-5 bottom-0">
+            <h3 className="text-[#2D2727]">Logout</h3>
+            <HiOutlineLogout size={25} className="text-[#2D2727]"/>
+          </div>
+          
+        </div>
 
       </nav>
 
