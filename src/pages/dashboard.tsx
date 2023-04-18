3bbad2a005731eae3390927ca9dd6c1ef490e831
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-18 19:53:07 +0800

    fix sidebar content bug

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { RxCross2,RxHamburgerMenu } from "react-icons/rx";
 import { Outlet, NavLink } from "react-router-dom";
+
 export default function Dashboard() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -8,13 +9,32 @@ export default function Dashboard() {
     setIsSidebarOpen(!isSidebarOpen);
   };
   return (
-    <div className="container min-h-screen max-w-screen-2xl flex bg-[#E1E1E1]">
+    <div className="container min-h-screen max-w-screen-2xl flex bg-white">
       <nav
-        className={` sidebar p-2  bg-[#262A56] ${
-          isSidebarOpen ? 'translate-x-0 flex-[6] shadow-2xl' : '-translate-x-full'
+        className={` sidebar bg-[#262A56] shadow-2xl ${
+          isSidebarOpen ? 'translate-x-0 flex-[6]' : '-translate-x-full'
         } transition-all duration-300 ease-in-out md:translate-x-0 md:flex-[2] lg:flex-1`}
-      ></nav>
-      <div className="main flex-[2] md:flex-[6] lg:flex-[4] p-5">
+      >
+
+        {/* MOBILE AND TABLET VIEW */}
+        { isSidebarOpen && (
+          <ul className="lg:hidden text-white">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+          </ul>
+        )}
+
+        {/* DESKTOP VIEW */}
+        <ul className="hidden lg:block text-white">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+        </ul>
+
+      </nav>
+
+      <div className="main flex-[2] md:flex-[6] lg:flex-[4] p-5 ">
         <button
           className="md:hidden"
           onClick={toggleSidebar}
