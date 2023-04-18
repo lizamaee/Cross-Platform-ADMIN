4cfd6a695ba5e7f1efd1e06d33b8d9f10255151d
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-18 23:05:35 +0800

    new createbrowserrouter syntax

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import DashHome from './pages/subpages/Home'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
-  createRoutesFromElements,
-  Route
 } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={ <App/>}>
-      <Route index element={ <Login/> }/>
-      <Route path='dashboard' element={ <Dashboard/> }/>
-    </Route>
-  )
-)
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [  
+      {
+        path: "",
+        element: <Login />
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard/>,
+        children: [
+          {
+            path: '',
+            element: <DashHome/>
+          }
+        ]
+      }
+    ],
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
