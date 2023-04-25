b6543ee2cf0a5974148493329db26ed89a8bbd9c
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-04-25 14:50:08 +0800

    add darkmode for ElectionTable

--- src/components/ElectionTable.tsx
+++ src/components/ElectionTable.tsx
@@ -19,27 +19,27 @@ interface Election {
 const ElectionTable: React.FC<ElectionTableProps>  = ({ election, handleElection, action, actionStyle, error }) => {
   
   return (
-    <table className="w-full h-full text-center pt-10 text-sm overflow-x-scroll drop-shadow-md mb-3">
+    <table className="w-full h-full text-center pt-10 text-sm overflow-x-scroll drop-shadow-md">
       <thead>
-        <tr className="pop-semibold text-sm py-2 dark:text-gray-200">
-          <th>Title</th>
-          <th>Status</th>
-          <th>Start Date</th>
-          <th>End Date</th>
-          <th>Action</th>
+        <tr className="pop-semibold border-t-2 border-neutral-200 dark:border-neutral-700 text-sm dark:text-gray-200">
+          <th className="py-3">Title</th>
+          <th className="py-3">Status</th>
+          <th className="py-3">Start Date</th>
+          <th className="py-3">End Date</th>
+          <th className="py-3">Action</th>
         </tr>
       </thead>
       <tbody>
       { error && (
         <tr>
-          <td colSpan={5} className="pop-semibold text-sm tracking-widest pt-5 opacity-50 dark:text-gray-200">{error}</td>
+          <td colSpan={5} className="pop-semibold text-sm tracking-widest py-5 opacity-50 dark:text-gray-200">{error}</td>
         </tr>
       )}
       { !error && election?.map((entry, index) => (
         <tr
           key={index}
-          className={` rounded-md align-middle ${
-            index % 2 === 0 ? "bg-[#eaf4fc]" : "bg-white dark:bg-[#4a4a4a]"
+          className={` rounded-md align-middle dark:text-gray-200 ${
+            index % 2 === 0 ? "bg-[#eaf4fc] dark:bg-[#2B2B2B]" : "bg-white dark:bg-[#333333]"
           }`}
         >
           <td className="py-5">{entry.title}</td>
