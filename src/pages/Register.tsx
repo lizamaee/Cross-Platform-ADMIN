787f2dd9eb7889dbb12955c824f025df1442b6ce
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-05-24 11:51:41 +0800

    add Register Page validations

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,13 +5,18 @@ import { useAuthStore } from "../hooks/state";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEyeSlash,FaEye } from 'react-icons/fa';
 import { message } from 'antd'
+import { z, ZodType } from 'zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
+type RegisterFormData = {
+  student_id: string;
+  password: string;
+  confirmPassword: string;
+  mobile_number: string;
+}
 
-export default function Login() {
-  const [id, setId] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [password, setPassword] = useState(""); 
-  const [samePassword, setSamePassword] = useState(""); 
+export default function Register() {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false)
   const [showPassword, setShowPassword] = useState(false);
@@ -19,13 +24,29 @@ export default function Login() {
   const navigate = useNavigate();
 
 
+  const schema: ZodType<RegisterFormData> = z.object({
+    student_id: z.string().regex(/^\d{7}$/, {message: "Student ID must be a valid Student ID"}).min(7).max(7),
+    password: z.string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*])/, {
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special character",
+    }).min(14, {message: "Password must contain at least 14 character(s)"}).max(30),
+    confirmPassword: z.string(),
+    mobile_number: z.string().regex(/^09\d{9}$/, {message: "Mobile number must be a valid PH Mobile Number",
+    }).min(11).max(11)
 
-  const handleRegister = async (e: any) => {
-    e.preventDefault();
+  }).refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"]
+  })
+
+  const {register, handleSubmit, formState:{errors}} = useForm<RegisterFormData>({resolver: zodResolver(schema)})
+
+
+  const handleRegister = async (data: RegisterFormData) => {
     setError(null)
     setIsLoading(true)
     try {
-      const resId = await checkStudentID(id) 
+      const resId = await checkStudentID(data.student_id) 
         .then(() => {
             message.open({
                 type: 'loading',
@@ -34,8 +55,7 @@ export default function Login() {
               }).then(() => message.success("Eligible to Register :)", 2.5))
         })
       
-
-      const philFormat = mobileNumber.slice(1)
+      const philFormat = data.mobile_number.slice(1)
 
       const resNum = await confirmNumberSendOTP(`+63${philFormat}`)
         .then(() => {
@@ -46,9 +66,9 @@ export default function Login() {
           }).then(() => message.success("Happy Registrations :)", 2.5))
         })
 
-      useAuthStore.setState({ tempMobileNumber: mobileNumber })
-      useAuthStore.setState({ tempPassword: password })
-      useAuthStore.setState({ student_id: id })
+      useAuthStore.setState({ tempMobileNumber: data.mobile_number })
+      useAuthStore.setState({ tempPassword: data.password })
+      useAuthStore.setState({ student_id: data.student_id })
 
       //console.log(resId.data)
       //console.log(resNum.data)
@@ -63,7 +83,7 @@ export default function Login() {
         }else{
           setIsLoading(false)
           //console.log(err.response.data);
-          const philFormat = mobileNumber.slice(1)
+          const philFormat = data.mobile_number.slice(1)
           if(err?.response.data.error === `Invalid parameter \`To\`: +63${philFormat}`){
             setError("Invalid phone number. Please provide a valid Philippine phone number.")
           }else{
@@ -84,7 +104,7 @@ export default function Login() {
   return (
     <div className="md:flex md:items-center md:justify-center md:min-h-screen md:p-10 font-medium text-lg">
       <form
-        onSubmit={handleRegister}
+        onSubmit={handleSubmit(handleRegister)}
         className=" bg-[#E5E0FF] dark:bg-[#2b2b2b] dark:text-gray-200 md:rounded-3xl overflow-hidden text-[#3C486B] pb-5 md:shadow-2xl md:max-w-md"
       >
         <img className="" src={tcu} height={400} alt="Taguig City University" />
@@ -96,13 +116,13 @@ export default function Login() {
             <input
               className="bg-[#E5E0FF] px-4 py-3 mb-2 rounded-lg text-black text-md pop-medium outline-none border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a] dark:text-white tracking-wider"
               type="text"
-              value={id}
+              {...register("student_id")}
               placeholder="ex. 1234567"
-              onChange={(e) => setId(e.target.value)}
               maxLength={7}
               minLength={7}
               required
             />
+            {errors.student_id && <span className="text-red-400 text-center text-sm">{errors.student_id.message}</span>}
           </div>
 
           <div className="flex flex-col px-5">
@@ -112,9 +132,8 @@ export default function Login() {
               <input
                 className="bg-[#E5E0FF] w-full px-4 py-3 rounded-lg text-black text-md pop-medium outline-none border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a] dark:text-white tracking-wider"
                 type={showPassword ? "text" : "password"}
-                value={password}
+                {...register("password")}
                 placeholder="••••••••"
-                onChange={(e) => setPassword(e.target.value)}
                 required
               />
               <button
@@ -124,6 +143,7 @@ export default function Login() {
               >
                 {showPassword ? <FaEyeSlash size={23}/> : <FaEye size={23}/>}
               </button>
+              {errors.password && <span className="text-red-400 text-center text-sm w-[100%]">{errors.password.message}</span>}
             </div>
             {/* CONFIRM PASSWORD */}
             <label className="pop-regular opacity-80 text-sm">Confirm Password (required)</label>
@@ -131,9 +151,8 @@ export default function Login() {
               <input
                 className="bg-[#E5E0FF] w-full px-4 py-3 rounded-lg text-black text-md pop-medium outline-none border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a] dark:text-white tracking-wider"
                 type={showSamePassword ? "text" : "password"}
-                value={samePassword}
+                {...register("confirmPassword")}
                 placeholder="••••••••"
-                onChange={(e) => setSamePassword(e.target.value)}
                 required
               />
               <button
@@ -143,38 +162,39 @@ export default function Login() {
               >
                 {showSamePassword ? <FaEyeSlash size={23}/> : <FaEye size={23}/>}
               </button>
+              {errors.confirmPassword && <span className="text-red-400 text-center text-sm">{errors.confirmPassword.message}</span>}
             </div>
             {/* MOBILE NUMBER */}
             <label className="pop-regular opacity-80 text-sm">Mobile Number (required)</label>
             <input
-              className="bg-[#E5E0FF] px-4 py-3 md:mb-2 rounded-lg text-black text-md pop-medium outline-none border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a] dark:text-white tracking-wider"
+              className="bg-[#E5E0FF] px-4 py-3 mb-2 rounded-lg text-black text-md pop-medium outline-none border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a] dark:text-white tracking-wider"
               type="text"
-              value={mobileNumber}
+              {...register("mobile_number")}
               placeholder="ex. 09123456789"
-              onChange={(e) => setMobileNumber(e.target.value)}
               maxLength={11}
               minLength={11}
               required
             />
+            {errors.mobile_number && <span className="text-red-400 text-center text-sm">{errors.mobile_number.message}</span>}
           </div>
         </div>
         <div className="flex items-center flex-col px-5">
           {error ? (
-            <div className="text-red-400 text-center px-5 py-3 break-words">
+            <div className="text-red-400 text-center text-sm px-5 py-2 break-words">
               {error}
             </div>
           ) : (
             ""
           )}
           {isLoading ? (<button
-            className="text-center text-white px-4 py-3 rounded-lg w-[100%] bg-[#4C7CE5] dark:bg-[#4C7CE5] shadow-md break-words shadow-blue-300 dark:shadow-blue-400"
+            className="text-center text-white mt-2 px-4 py-3 rounded-lg w-[100%] bg-[#4C7CE5] dark:bg-[#4C7CE5] shadow-md break-words shadow-blue-300 dark:shadow-blue-400"
             type="submit"
           >
             Loading...
           </button>)
           :
           (<button
-            className="text-center text-white px-4 py-3 rounded-lg w-[100%] bg-[#4C7CE5] dark:bg-[#4C7CE5] shadow-md break-words shadow-blue-300 dark:shadow-blue-400"
+            className="text-center text-white mt-2 px-4 py-3 rounded-lg w-[100%] bg-[#4C7CE5] dark:bg-[#4C7CE5] shadow-md break-words shadow-blue-300 dark:shadow-blue-400"
             type="submit"
           >
             Register
