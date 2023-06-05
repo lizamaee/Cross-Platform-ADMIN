794cee5ea47e1fd9060cb74617eb3afdf881219d
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-05 23:24:38 +0800

    fix ElectionTab Logic

--- src/pages/adminsubpages/election/ElectionTab.tsx
+++ src/pages/adminsubpages/election/ElectionTab.tsx
@@ -9,7 +9,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from 'moment';
 import { DownOutlined } from '@ant-design/icons';
-import { useAvailableOrganizations, useCreateElection, useDeleteElection, useElections } from '../../../hooks/queries/useElection';
+import { useCreateElection, useDeleteElection, useElections, useUpdateElection } from '../../../hooks/queries/useElection';
 
 interface DataType {
   id: string;
@@ -32,16 +32,18 @@ export default function ElectionTab() {
   const navigate = useNavigate()
   const location = useLocation()
   
-  //Queries
+  //ELECTOIN QUERY HOOKS
+  //GET ALL
   const electionsQuery = useElections()
+  //CREATE SINGLE
   const { mutate: createElection} = useCreateElection()
+  //DELETE SINGLE
   const { mutate: deleteElection} = useDeleteElection()
-
-  //console.log(electionsQuery.data);
   
-  const sortedReverse = electionsQuery.data?.sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+  //SORT ELECTION ARRAY IN DESCENDING ORDER
+  const descendingElections = electionsQuery?.data?.sort((a:any, b:any) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
 
-  
+  //FETCH DATA FOR CURRENT AND AVAILABLE ORGANIZATIONS
   const fetchData = async (endpoints: string) => {
     try {
       const response = await axiosPrivate.get(`/${endpoints}`);
@@ -63,12 +65,12 @@ export default function ElectionTab() {
   }
   
 
-  //delete election handler
+  //ASYNCRONOUS DELETE ELECTION FUNCTION
   async function handleDeleteElection(id: string) {
     deleteElection(id)
   }
 
-  //confirm delete modal
+  //DELETE ELECTION CONFIRMATION MODAL
   const deleteIt = (id: string, name: string) => {
     Modal.confirm({
       title: 'Do you want to delete this Election?',
@@ -85,11 +87,14 @@ export default function ElectionTab() {
     });
   }
 
-
+  //OPEN CREATE DRAWER STATE
   const [open, setOpen] = useState(false)
+  //OPEN CREATE DRAWER FUNCTION
   const showDrawer = () => {
     setOpen(true)
   }
+  
+  //CLOSE CREATE DRAWER FUNCTION
   const onClose = () => {
     setName('')
     setStartDate(null)
@@ -97,6 +102,7 @@ export default function ElectionTab() {
     setOpen(false)
   }
 
+  //CREATE ELECTION FUNCTION
   const handleCreateElection = async () => {
     setIsCreating(true)
     if(startDate === null || endDate === null || name === '') {
@@ -108,21 +114,18 @@ export default function ElectionTab() {
         duration: 2.5,
       });
     }
-
-    //console.log(logoCloudLink);
-    
     const uploadElecData = {
       title: name,
       startDate: moment(startDate).utc().format('YYYY-MM-DD'),
       endDate: moment(endDate).utc().format('YYYY-MM-DD')
     }
-
     //mutate the election data
     createElection(uploadElecData)
     onClose()
     setIsCreating(false)
   }
 
+  //DRAWER STATES
   const [isUpdating, setIsUpdating] = useState<boolean>(false)
   const [modifyId, setModifyId] = useState<string>('')
   const [modifyName, setModifyName] = useState<string>('')
@@ -135,13 +138,14 @@ export default function ElectionTab() {
   const [isDisconnectiong, setIsDisConnecting] = useState<boolean>(false)
   const [isConnectiong, setIsConnecting] = useState<boolean>(false)
   
+  //OPEN MODIFY MULTI-PARENT DRAWER FUNCTION
   const openMultiDrawer = (id: string, name: string) => {
     setOpenMulti(true)
     const single = electionsQuery.data?.filter((elec: DataType)=> elec.id === id)
-
+    
     const startdate = new Date(single[0].startDate)
     const enddate = new Date(single[0].endDate)
-
+    
     setModifyId(single[0].id)
     setModifyName(single[0].title)
     setModifyStatus(single[0].status)
@@ -150,12 +154,18 @@ export default function ElectionTab() {
     
   }
   
+  //CLOSE MODIFY MULTI-PARENT DRAWER FUNCTION
   const onCloseMultiDrawer = () => {
     setOpenMulti(false)
   }
   
-  const availableQueary = useAvailableOrganizations()
+  //GET ALL AVAILABLE ORGANIZATIONS REQUEST FUNCTION
+  const getAvailableOrganizations = async () => {
+    const response = await fetchData('organization/null')
+    setAvailableOrganizations(response)
+  }
 
+  //GET ALL CURRENT ORGANIZATIONS REQUEST FUNCTION
   const getCurrentOrganizations = async () => {
     const response = await fetchData(`election/elec-organizations/${modifyId}`)
     if(response[0]?.error === 'Network Error'){
@@ -166,11 +176,11 @@ export default function ElectionTab() {
           duration: 2.5,
         })
     }
-    //console.log(response)
     setElectionID(response.id)
     setCurrentOrganizations(response)
   }
 
+  //DISCONNECT SINGLE ORGANIZATION FROM SPECIFIC ELECTION
   const disConnectOrganization = async (organizationId: string, electionId: string) => {
     setIsDisConnecting(true)
 
@@ -182,13 +192,13 @@ export default function ElectionTab() {
 
     await axiosPrivate.patch('election/disconnect-org-election', disconnectOrganization)
     .then((response) => {
-        //console.log('Success:', response.data);
         message.open({
           key,
           type: 'success',
           content: 'Disconnected Successfully',
           duration: 2,
         });  
+        getAvailableOrganizations()
         getCurrentOrganizations()
         setIsDisConnecting(false)
     })
@@ -223,16 +233,22 @@ export default function ElectionTab() {
     });
   }
 
+  //OPEN MODIFY MULTI-CHILD DRAWER FUNCTION
   const showChildrenDrawer = () => {
+    getAvailableOrganizations()
     getCurrentOrganizations()
     setChildrenDrawer(true);
   };
   
+  //CLOSE MODIFY MULTI-CHILD DRAWER FUNCTION
   const onChildrenDrawerClose = () => {
     setChildrenDrawer(false);
   };
 
-  const updateOrganization = async (e:any) => {
+  //MUTATION HOOK OF UPDATE ELECTION 
+  const {mutate: updateElection} = useUpdateElection()
+  //UPDATE AN ELECTION FUNCTION
+  const handleUpdate = async (e:any) => {
     e.preventDefault()
     setIsUpdating(true)
     
@@ -246,79 +262,42 @@ export default function ElectionTab() {
       });
     }
 
-    const key = 'updateElectionKEY'
     const updateElecData = {
+      id: modifyId,
       title: modifyName,
-      start_date: moment(modifyStartDate).utc().format('YYYY-MM-DD'),
-      end_date: moment(modifyEndDate).utc().format('YYYY-MM-DD')
+      startDate: moment(modifyStartDate).utc().format('YYYY-MM-DD'),
+      endDate: moment(modifyEndDate).utc().format('YYYY-MM-DD')
     }
 
-    await axiosPrivate.patch(`/election/single/${modifyId}`, updateElecData)
-    .then((response) => {
-        //console.log('Success:', response.data);
-        message.open({
-          key,
-          type: 'success',
-          content: 'Election Updated',
-          duration: 2,
-        });
-        onCloseMultiDrawer()
-        setIsUpdating(false)
-        setModifyId('')
-        setModifyName('')
-        setModifyStartDate(null)
-        setModifyEndDate(null)    
-    })
-    .catch((error) => {
-        //console.error('Error:', error);
-        setIsUpdating(false)
-        if (error.message === 'Network Error') {
-          message.open({
-            type: 'error',
-            content: 'Server Unavailable',
-            className: 'custom-class pop-medium',
-            duration: 2.5,
-          });
-        } else if(error.response.data?.message){
-          message.open({
-            type: 'error',
-            content: `${error.response.data.message}`,
-            className: 'custom-class pop-medium',
-            duration: 2.5,
-          });
-        }else {
-          // Handle other errors
-          error.response.data.errors?.map((err:any) => {
-            message.open({
-              type: 'error',
-              content: `${err.msg}`,
-              className: 'custom-class pop-medium',
-              duration: 2.5,
-            })
-          })
-        }
-    });
+    updateElection(updateElecData)
+    setIsUpdating(false)
+    onCloseMultiDrawer()
+    setModifyId('')
+    setModifyName('')
+    setModifyStartDate(null)
+    setModifyEndDate(null)    
+
   }
 
-
+  //CONNECT SINGLE ORGANIZATION FROM SPECIFIC ELECTION
   const onClick: MenuProps['onClick'] = async ({ key }) => {
     setIsConnecting(true)
 
-    const messageKey = 'connectPositionKEY'
-    const connectPosition = {
+    const messageKey = 'connectOrganizationKEY'
+    const connectOrganization = {
       election_id: modifyId,
       org_id: key,
     }
 
-    await axiosPrivate.patch('election/connect-org-election', connectPosition)
+    await axiosPrivate.patch('election/connect-org-election', connectOrganization)
     .then((response) => {
-        //console.log('Success:', response.data);
         message.open({
           key: messageKey,
           type: 'success',
           content: 'Connected Successfully',
           duration: 2,
         });  
+        getAvailableOrganizations()
         getCurrentOrganizations()
         setIsConnecting(false)
     })
@@ -354,7 +333,8 @@ export default function ElectionTab() {
 
   };
 
-  const items: MenuProps['items'] = availableQueary.data?.length === 0 
+  //POPULATE AVAILABLE ORGANIZATION INSIDE A DROPDOWN
+  const items: MenuProps['items'] = availableOrganizations?.length === 0 
     ? [{
         label: "No Organizations Available",
         key: "No Key"
@@ -382,15 +362,16 @@ export default function ElectionTab() {
       <div className="container w-full mx-auto p-4 overflow-x-auto">
         {electionsQuery.status === 'error' || electionsQuery.data?.[0]?.error === 'Network Error'
             ? <h4 className='text-red-400 pop-medium py-4 text-center text-xs md:text-sm tracking-wide flex-1'>Sorry, Something went wrong.</h4>
-            : sortedReverse.length === 0 
+            : descendingElections?.length === 0 
                 ? <h4 className='text-gray-400 opacity-90 border-2 rounded-lg pop-medium py-4 text-center text-xs md:text-sm tracking-wide flex-1'>No Elections</h4>
                 : <div className="grid items-center gap-5">
-                  {sortedReverse.map((elec:DataType, index: any) =>(
+                  {descendingElections?.map((elec:DataType, index: any) =>(
                     <div key={index} className="card px-4 shadow-md bg-gray-100 rounded-md dark:bg-[#2a2a2a] flex flex-cols-2">
                       <div className="grow gap-3 flex flex-col md:flex-row md:justify-between md:pr-20 justify-center">
                         <div className="election-title flex justify-center md:justify-start items-center">
                           <h3 className='pop-semibold text-[#303030] dark:text-gray-200 text-center '>{elec.title}</h3>
                         </div>
+                        {/* DATE DISPLAY */}
                         <div className="dates flex text-xs gap-3 items-center justify-center text-gray-500 pop-regular">
                           <p className="">
                             {new Date(elec.startDate).toLocaleDateString("en-US", {
@@ -410,13 +391,15 @@ export default function ElectionTab() {
                             })}
                           </p>
                         </div>
+                        {/* DATE DISPLAY */}
                       </div>
+                      {/* ACTIONS DISPLAY */}
                       <div className="actions flex flex-col-reverse md:flex-row items-end justify-end gap-3 md:gap-5 py-2">
                         <Tooltip title='Delete' color='#f87171'>
                           <button
                             onClick={(e) => deleteIt(elec.id, elec.title)}
                             className={`pop-medium text-center align-middle p-1 md:p-2 rounded-lg md:rounded-xl text-white bg-red-400 shadow-sm shadow-red-400 focus:outline-none`}
-                          >
+                            >
                             <RiDeleteBin5Fill />
                           </button>
                         </Tooltip>
@@ -432,12 +415,14 @@ export default function ElectionTab() {
                           </button>
                         </Tooltip>
                       </div>
+                      {/* ACTIONS DISPLAY */}
                     </div>
                   ))}
             </div>
             }
         
       </div>
+      {/* CREATE ELECTION DRAWER */}
       <Drawer title="Create Election" placement="right" onClose={onClose} open={open}>
         <form className="create-election-container py-3">
           <div className="name flex flex-col pop-medium">
@@ -482,7 +467,7 @@ export default function ElectionTab() {
                   className="pop-regular shadow-sm bg-gray-100 border-2 w-[90%] md:w-[85%] border-slate-200 rounded-md focus:outline-indigo-400 px-4"
                   selected={endDate ?? null}
                   onChange={(date) => setEndDate(date)}
-                />
+                  />
               </div>
             </div>
             {/* DATE PICKER */}
@@ -504,11 +489,15 @@ export default function ElectionTab() {
         )}
         {/* CREATE BUTTON */}
       </Drawer>
+      {/* CREATE ELECTION DRAWER */}
 
+      {/* UPDATE ELECTION MULTI-PARENT DRAWER */}
       <Drawer title="Modify" onClose={onCloseMultiDrawer} open={openMulti}>
         <div className='pop-medium'>
+          {/* CONNECTIONS BUTTON */}
           <button onClick={showChildrenDrawer} className='border-2 py-1 px-2 rounded-md'>Connections</button>
-          <label className='pb-1 opacity-80 mt-8'>Election Name</label>
+          {/* CONNECTIONS BUTTON */}
+          <label className='pb-1 opacity-80 pt-8 block'>Election Name</label>
           <input value={modifyName} onChange={(e) => setModifyName(e.target.value)} type="text" className='py-1 px-3 text-lg focus:outline-indigo-400 rounded-md border-solid border-2 w-full' />
 
           {/* DATE PICKER */}
@@ -529,7 +518,7 @@ export default function ElectionTab() {
                   endDate={modifyEndDate}
                   placeholderText="nothing!"
                   className="pop-regular shadow-sm bg-gray-100 border-2 w-[90%] md:w-[85%] border-slate-200 rounded-md focus:outline-indigo-400 px-4"
-                />
+                  />
               </div>
               {/* END DATE */}
               <div className="end">
@@ -552,10 +541,10 @@ export default function ElectionTab() {
               </div>
             </div>
             {/* DATE PICKER */}
-
+            {/* UPDATE BUTTON */}
             <div className="cnt flex items-center justify-center p-5">
               {!isUpdating 
-                ? <button className='flex items-center border-2 border-[#1677ff] text-[#1677ff] py-2 px-7 rounded-full' onClick={updateOrganization}>
+                ? <button className='flex items-center border-2 border-[#1677ff] text-[#1677ff] py-2 px-7 rounded-full' onClick={handleUpdate}>
                     <p className='pop-medium'>Update</p>   
                   </button>
                 : <button disabled={isUpdating} className='flex pop-medium items-center border-2 border-[#1677ff] text-[#1677ff] py-2 px-3 rounded-full'>
@@ -564,13 +553,15 @@ export default function ElectionTab() {
                   </button>
               }
             </div>
+            {/* UPDATE BUTTON */}
           </div>
+        {/* UPDATE ELECTION MULTI-CHILD DRAWER */}
         <Drawer
           title="Connections"
           width={320}
           onClose={onChildrenDrawerClose}
           open={childrenDrawer}
-        >
+          >
           <div className="wrap border-2 w-fit p-1 rounded-lg pop-medium">
             <Dropdown menu={{ items, onClick }}>
               <a onClick={(e) => e.preventDefault()}>
@@ -600,7 +591,9 @@ export default function ElectionTab() {
             }
           </div>
         </Drawer>
+        {/* UPDATE ELECTION MULTI-CHILD DRAWER */}
       </Drawer>
+      {/* UPDATE ELECTION MULTI-PARENT DRAWER */}
       
       {/* ALL ORGANIZATIONS */}
     </div>
