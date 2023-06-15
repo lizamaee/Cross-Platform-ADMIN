c52a8ca6a0ca6722884304fc20e0266f921f3839
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-15 20:40:54 +0800

    add upload new voter info query @ useVoter hook

--- src/hooks/queries/useVoter.ts
+++ src/hooks/queries/useVoter.ts
@@ -109,3 +109,112 @@ export const useDeleteAccount = () => {
       }
   })
 }
+
+
+//VOTER
+//QUERY FOR UPLOADING VOTER PROFILE PICTURE
+export const useUploadVoterPicture = () => {
+  const queryClient = useQueryClient()
+  const axiosPrivate = useAxiosPrivate()
+  return useMutation({
+      mutationFn:async (newData: {student_id:string, profile_picture: string,}) => {
+          const response = await axiosPrivate.patch(`/change-student-picture`, {
+            student_id: newData.student_id,
+            new_profile_picture: newData.profile_picture,
+          } )
+          return response.data
+      },
+      onSuccess: async () => {
+          message.open({
+              key: 'successCreation',
+              type: 'success',
+              content: 'Profile Picture Uploaded :)',
+              duration: 2,
+          })
+          await queryClient.invalidateQueries({
+              queryKey: ['voter'],
+              exact: true
+          })
+      },
+      onError: (error:any) => {
+          if (error.message === 'Network Error') {
+              message.open({
+                type: 'error',
+                content: 'Server Unavailable',
+                className: 'custom-class pop-medium',
+                duration: 2.5,
+              });
+            } else if(error.response.data?.error){
+              message.open({
+                type: 'error',
+                content: `${error.response.data.error}`,
+                className: 'custom-class pop-medium',
+                duration: 2.5,
+              });
+            }else {
+              // Handle other errors
+              error.response.data.errors?.map((err:any) => {
+                message.open({
+                  type: 'error',
+                  content: `${err.msg}`,
+                  className: 'custom-class pop-medium',
+                  duration: 2.5,
+                })
+              })
+            }
+      }
+  })
+}
+//QUERY FOR UPLOADING VOTER PROFILE PICTURE
+export const useUploadVoterInfo = () => {
+  const queryClient = useQueryClient()
+  const axiosPrivate = useAxiosPrivate()
+  return useMutation({
+      mutationFn:async (newData: {student_id:string, fullname: string,}) => {
+          const response = await axiosPrivate.patch(`/new-voter`, {
+            student_id: newData.student_id,
+            fullname: newData.fullname,
+          } )
+          return response.data
+      },
+      onSuccess: async () => {
+          message.open({
+              key: 'successCreation',
+              type: 'success',
+              content: 'Profile Picture Uploaded :)',
+              duration: 2,
+          })
+          await queryClient.invalidateQueries({
+              queryKey: ['voter'],
+              exact: true
+          })
+      },
+      onError: (error:any) => {
+          if (error.message === 'Network Error') {
+              message.open({
+                type: 'error',
+                content: 'Server Unavailable',
+                className: 'custom-class pop-medium',
+                duration: 2.5,
+              });
+            } else if(error.response.data?.error){
+              message.open({
+                type: 'error',
+                content: `${error.response.data.error}`,
+                className: 'custom-class pop-medium',
+                duration: 2.5,
+              });
+            }else {
+              // Handle other errors
+              error.response.data.errors?.map((err:any) => {
+                message.open({
+                  type: 'error',
+                  content: `${err.msg}`,
+                  className: 'custom-class pop-medium',
+                  duration: 2.5,
+                })
+              })
+            }
+      }
+  })
+}
