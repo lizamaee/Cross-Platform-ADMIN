ef173e2f1b97d753c9c9832ee29c054d9ed6f1d8
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-20 17:08:55 +0800

    add vite environment API_URL variable

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL
 
 export const loginadmin = async (student_id: string, password: string) => {
   try {
-    const res = await axios.post('http://localhost:3000/login-primary', 
+    const res = await axios.post(`${apiUrl}/login-primary`, 
     JSON.stringify({ student_id, password }),
     {
       headers: { "Content-Type": "application/json"},
@@ -16,7 +17,7 @@ export const loginadmin = async (student_id: string, password: string) => {
 };
 export const loginFinaly = async (student_id: string, password: string) => {
   try {
-    const res = await axios.post('http://localhost:3000/login', 
+    const res = await axios.post(`${apiUrl}/login`, 
     JSON.stringify({ student_id, password }),
     {
       headers: { "Content-Type": "application/json"},
@@ -30,7 +31,7 @@ export const loginFinaly = async (student_id: string, password: string) => {
 };
 export const forgotPasswordSendOTP = async (mobile_number: string) => {
   try {
-    const res = await axios.get('http://localhost:3000/forgot-password-send',{params:{ mobile_number} })
+    const res = await axios.get(`${apiUrl}/forgot-password-send`,{params:{ mobile_number} })
     return res;
   } catch (err) {
     throw err;
@@ -38,7 +39,7 @@ export const forgotPasswordSendOTP = async (mobile_number: string) => {
 };
 export const forgotPassword = async (mobile_number: string, new_password: string) => {
   try {
-    const res = await axios.patch('http://localhost:3000/forgot-password',{ mobile_number, new_password})
+    const res = await axios.patch(`${apiUrl}/forgot-password`,{ mobile_number, new_password})
     return res;
   } catch (err) {
     throw err;
@@ -47,7 +48,7 @@ export const forgotPassword = async (mobile_number: string, new_password: string
 
 export const checkStudentID = async (student_id: string) => {
   try {
-    const res = await axios.get('http://localhost:3000/id', { params: { student_id } });
+    const res = await axios.get(`${apiUrl}/id`, { params: { student_id } });
     return res;
   } catch (error) {
     throw error;
@@ -56,7 +57,7 @@ export const checkStudentID = async (student_id: string) => {
 
 export const confirmNumberSendOTP = async (new_number: string) => {
   try {
-    const res = await axios.post('http://localhost:3000/otp/send', 
+    const res = await axios.post(`${apiUrl}/otp/send`, 
     JSON.stringify({ mobile_number: new_number }),
     {
       headers: { "Content-Type": "application/json"},
@@ -70,7 +71,7 @@ export const confirmNumberSendOTP = async (new_number: string) => {
 }
 export const confirmNumberVerifyOTP = async (mobile_number: string, otp_code: string) => {
   try {
-    const res = await axios.post('http://localhost:3000/otp/verify', 
+    const res = await axios.post(`${apiUrl}/otp/verify`, 
     JSON.stringify({ mobile_number, otp_code }),
     {
       headers: { "Content-Type": "application/json"},
@@ -86,7 +87,7 @@ export const confirmNumberVerifyOTP = async (mobile_number: string, otp_code: st
 export const registerUser = async (student_id: string, password: string, pin_number: string, mobile_number: string) => {
 
   try {
-    const res = await axios.post('http://localhost:3000/register', 
+    const res = await axios.post(`${apiUrl}/register`, 
     JSON.stringify({ student_id, password, pin_number, mobile_number}),
     {
       headers: { "Content-Type": "application/json"},
