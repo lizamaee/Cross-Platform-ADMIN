c388521e0387b2b848722ef9369a8df3e4e7dcf1
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-20 11:23:34 +0800

    add invalidata voter queryKey @ useLogout hook

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,15 +1,21 @@
+import { useQueryClient } from "@tanstack/react-query"
 import axios from "../api/axios"
 import { useAuthStore } from "./state"
 
 
 export default function useLogout() {
     const {setToken, student_id} = useAuthStore((state) => state)
+    const queryClient = useQueryClient()
     const logout = async () => {
         setToken({})
         try {
             if(student_id){
                 const response = await axios.post('/logout',{student_id}, {
                     withCredentials: true
+                })
+                await queryClient.invalidateQueries({
+                    queryKey: ['voter'],
+                    exact: true
                 })
             }
             useAuthStore.setState({student_id: ''})
