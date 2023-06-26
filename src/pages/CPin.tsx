173cf517b4f09e2180d224da52a18b439ddd9ec1
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-06-26 21:29:23 +0800

    fix responsivenes @ CPin

--- src/pages/CPin.tsx
+++ src/pages/CPin.tsx
@@ -17,12 +17,12 @@ type PinFormData = {
 export default function CPin() {
   const [isConfirming, setIsConfirming] = useState(false);
   const [showPin, setShowPin] = useState(false);
-  const [showConfirmPin, setShowConfrimPin] = useState(false);
+  const [showConfirmPin, setShowConfirmPin] = useState(false);
   const {tempPassword, tempMobileNumber, student_id} = useAuthStore((state) => state)
   const navigate = useNavigate()
 
   const handleShowPin = () => setShowPin(!showPin);
-  const handleShowConfrimPin = () => setShowConfrimPin(!showConfirmPin);
+  const handleShowConfirmPin = () => setShowConfirmPin(!showConfirmPin);
 
   const schema: ZodType<PinFormData> = z
     .object({
@@ -100,65 +100,65 @@ export default function CPin() {
   };
 
   return (
-    <div className="flex justify-center">
+    <div className="flex w-full justify-center">
       <div className=" flex flex-col px-5 md:w-[40%] pt-20">
         <BsFillShieldLockFill
           size={100}
           className="text-center w-full text-[#4C7CE5] pb-5"
         />
-        <h2 className="text-[#4C7CE5] text-lg md:text-2xl pb-10 md:pb-20 text-center pop-bold">
+        <h2 className="text-[#4C7CE5] sm:text-lg md:text-2xl pb-10 md:pb-20 text-center pop-bold">
           Create your PIN code
         </h2>
-        <form onClick={handleSubmit(handleConfirm)} className="flex flex-col">
+        <form onSubmit={handleSubmit(handleConfirm)} className="flex flex-col">
           {/* CREATE PIN CODE */}
-          <label className="text-[#3F3D56] dark:text-gray-400 pop-light py-2 opacity-80 text-sm">
+          <label className="text-[#3F3D56] sm:text-sm dark:text-gray-400 pop-light py-2 opacity-80 text-xs">
             PIN code
           </label>
-          <div className="flex rounded-lg bg-[#E5E0FF] w-full border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a]">
-            <input
-              className="grow pl-4 py-3 text-black text-md pop-medium outline-none  dark:text-white tracking-wider bg-transparent"
-              type={showPin ? "text" : "password"}
-              {...register("pin_code")}
-              placeholder="ex. 1234"
-              maxLength={4}
-              minLength={4}
-              required
-            />
-            <button
-              className="flex justify-center items-center text-sm font-bold w-14"
-              type="button"
-              onClick={handleShowPin}
-            >
-              {showPin ? <FaEyeSlash size={23} /> : <FaEye size={23} />}
-            </button>
-          </div>
+          <div className="flex text-gray-800 dark:text-gray-400 rounded-lg relative bg-[#E5E0FF] w-full border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a]">
+              <input
+                className="w-5/6 sm:w-full pl-4 py-3 text-black text-md pop-medium outline-none  dark:text-white tracking-wider bg-transparent"
+                type={showPin ? "text" : "password"}
+                {...register("pin_code")}
+                placeholder="Ex. 1234"
+                maxLength={4}
+                minLength={4}
+                required
+              />
+              <button
+                className="flex absolute sm:static z-20 top-3 right-0 justify-center items-center text-sm font-bold w-14"
+                type="button"
+                onClick={handleShowPin}
+              >
+                {showPin ? <FaEyeSlash size={23}/> : <FaEye size={23}/>}
+              </button>
+            </div>
           {errors.pin_code && (
             <span className="text-red-400 text-center text-sm">
               {errors.pin_code.message}
             </span>
           )}
           {/* CONFIRM PIN CODE */}
-          <label className="text-[#3F3D56] dark:text-gray-400 pop-light py-2 opacity-80 text-sm">
+          <label className="text-[#3F3D56] sm:text-sm dark:text-gray-400 pop-light py-2 opacity-80 text-xs">
             Confirm PIN code
           </label>
-          <div className="flex rounded-lg bg-[#E5E0FF] w-full border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a]">
-            <input
-              className="grow pl-4 py-3 text-black text-md pop-medium outline-none  dark:text-white tracking-wider bg-transparent"
-              type={showConfirmPin ? "text" : "password"}
-              {...register("confirm_pin_code")}
-              placeholder="ex. 1234"
-              maxLength={4}
-              minLength={4}
-              required
-            />
-            <button
-              className="flex justify-center items-center text-sm font-bold w-14"
-              type="button"
-              onClick={handleShowConfrimPin}
-            >
-              {showConfirmPin ? <FaEyeSlash size={23} /> : <FaEye size={23} />}
-            </button>
-          </div>
+          <div className="flex text-gray-800 dark:text-gray-400 rounded-lg relative bg-[#E5E0FF] w-full border-solid border-2 border-gray-300 dark:border-gray-600 dark:bg-[#4a4a4a4a]">
+              <input
+                className="w-5/6 sm:w-full pl-4 py-3 text-black text-md pop-medium outline-none  dark:text-white tracking-wider bg-transparent"
+                type={showConfirmPin ? "text" : "password"}
+                {...register("confirm_pin_code")}
+                placeholder="Ex. 1234"
+                maxLength={4}
+                minLength={4}
+                required
+              />
+              <button
+                className="flex absolute sm:static z-20 top-3 right-0 justify-center items-center text-sm font-bold w-14"
+                type="button"
+                onClick={handleShowConfirmPin}
+              >
+                {showConfirmPin ? <FaEyeSlash size={23}/> : <FaEye size={23}/>}
+              </button>
+            </div>
           {errors.confirm_pin_code && (
             <span className="text-red-400 text-center text-sm">
               {errors.confirm_pin_code.message}
@@ -169,14 +169,14 @@ export default function CPin() {
               <button
                 type="submit"
                 disabled={isConfirming}
-                className=" w-full py-3 px-20 mt-5 pop-bold text-white rounded-lg text-lg bg-[#4C7CE5]"
+                className=" w-full py-3 sm:px-20 mt-5 pop-bold text-white rounded-lg sm:text-lg bg-[#4C7CE5]"
               >
                 Confirm
               </button>
             ) : (
               <button
                 disabled={isConfirming}
-                className=" w-full py-3 px-20 mt-5 pop-bold text-white rounded-lg text-lg bg-[#4C7CE5]"
+                className=" w-full py-3 sm:px-20 mt-5 pop-bold text-white rounded-lg sm:text-lg bg-[#4C7CE5]"
               >
                 Confirming...
               </button>
