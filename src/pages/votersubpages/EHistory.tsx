03c94cb58d1842c588f55075b9b19b187c61b39e
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-08-22 22:17:00 +0800

    add list of history UI for EHistory

--- src/pages/votersubpages/EHistory.tsx
+++ src/pages/votersubpages/EHistory.tsx
@@ -1,10 +1,16 @@
 import { useAuthStore } from "../../hooks/state"
 import { BsFillSunFill, BsMoonFill } from 'react-icons/bs'
 import {FiBell} from 'react-icons/fi'
+import {  useHistory } from "../../hooks/queries/useVoter"
+import { FaAngleRight } from "react-icons/fa"
 
 export default function EHistory() {
   
   const { isNight, switchMode, student_id } = useAuthStore((state) => state)
+
+  //PARTICIPATED ELECTIONS QUERY HOOK
+  const participatedElectionsQuery = useHistory(student_id)
+  
   return (
     <div>
       <h1 className='text-[#1c295d] dark:text-gray-300 text-xl pt-1 md:pt-3 text-center pop-bold'>Election History</h1>
@@ -21,9 +27,37 @@ export default function EHistory() {
       </div>
       {/* NOTIFICATION HEADER */}
 
-      <div className="history p-5 mt-3 rounded-lg dark:text-gray-300 bg-white dark:bg-[#313131]">
-        <div className="elections p-5">
-          <div className="h3 pop-medium">No election history.</div>
+      <div className="history shadow-md overflow-hidden sm:p-5 mt-3 rounded-lg text-gray-800 dark:text-gray-300 bg-white dark:bg-[#313131]">
+        <div className="elections sm:p-5">
+          <div className="h3 pop-medium">
+            {participatedElectionsQuery?.data?.elections?.length === 0 
+              ? <h3 className='text-center text-gray-400'>No election history</h3>
+              : participatedElectionsQuery?.data?.map((election:any) => (
+                
+                <div key={election.id} className="election shadow-md pop-medium flex flex-col px-4 py-2 rounded-lg bg-gray-200 dark:bg-zinc-700">
+                  <p className="text-[10px]">Title</p>
+                  <div className="title w-full pb-1 flex justify-between items-baseline">
+                    <h3 className='pop-semibold'>{election.title}</h3>
+                    <FaAngleRight className="w-6 h-6 text-gray-400"/>
+                  </div>
+                  <div className="w-full flex justify-between items-center">
+                    <p className="text-xs pop-light opacity-70 ">
+                      {new Date(election.startDate).toLocaleDateString(
+                        "en-US",
+                        {
+                          timeZone: "Asia/Manila",
+                          day: "numeric",
+                          month: "short",
+                          year: "numeric",
+                        }
+                      )}
+                    </p>
+                    <h5 className={`capitalize py-1 px-2 rounded-xl text-right text-sm ${election.status === "ended" ? 'text-gray-100 dark:text-gray-300 bg-red-300 dark:bg-red-700' : 'text-gray-100 dark:text-gray-300 bg-sky-300 dark:bg-sky-700'}`}>{election.status}</h5>
+
+                  </div>
+                </div>
+            ))}
+          </div>
         </div>
       </div>
 
