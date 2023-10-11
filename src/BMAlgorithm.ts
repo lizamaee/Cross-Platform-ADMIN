e73075405bf593946a1abfab534bcda697aa9fbb
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-10-11 23:33:19 +0800

    change core election winner funtion

--- src/BMAlgorithm.ts
+++ src/BMAlgorithm.ts
@@ -1,18 +1,34 @@
-
-export const winner = (candidates: string[]):string | undefined => {
-    let candidate;
-    let count = 0;
-  
-    for (let elem of candidates) {
-      if (count === 0) {
-        candidate = elem;
-      }
-      if (candidate === elem) {
-        count++;
-      } else {
-        count--;
-      }
+export const winner = (arr: string[]): string[] | null => {
+    if (arr.length === 0) {
+        return null
     }
-  
-    return candidate;
-}
+    
+      const stringCount: { [key: string]: number } = {};
+      
+      for (const str of arr) {
+        if (stringCount[str]) {
+            stringCount[str]++;
+        } else {
+            stringCount[str] = 1;
+        }
+      }
+    
+      let maxCount = 0;
+      let mostFrequentStrings: string[] = [];
+    
+      for (const str in stringCount) {
+        if (stringCount[str] > maxCount) {
+          maxCount = stringCount[str];
+          mostFrequentStrings = [str];
+        } else if (stringCount[str] === maxCount) {
+          mostFrequentStrings.push(str);
+        }
+      }
+
+      if(mostFrequentStrings.length > 1){
+        return mostFrequentStrings
+      }else{
+        return mostFrequentStrings;
+      }
+      
+  }
