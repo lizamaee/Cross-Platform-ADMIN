e0c9e82d26167546ca019b20666e2b59302356e3
DevNanad <74762919+DevNanad@users.noreply.github.com>
2023-12-04 15:39:50 +0800

    update icons and add credit section at mobile

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -2,15 +2,20 @@ import { useEffect, useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { RiSettings2Line } from "react-icons/ri";
 import { BiHomeAlt } from "react-icons/bi";
-import { BsCalendarEvent,BsListStars } from "react-icons/bs";
+import { BsCalendarEvent,BsFillSunFill,BsListStars, BsMoonFill } from "react-icons/bs";
 import { MdOutlineHowToVote } from "react-icons/md";
 import { HiOutlineLogout } from "react-icons/hi";
 import { Outlet, NavLink } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
 import { IoMenu } from "react-icons/io5";
+import { useAuthStore } from "../hooks/state";
+import { CgInfo } from "react-icons/cg";
+import DeleteMe from "../components/DeleteMe";
 
 export default function Dashboard() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { isNight, switchMode } = useAuthStore((state) => state)
+  const [isOpenCredit, setIsOpenCredit] = useState(false);
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
@@ -38,6 +43,26 @@ export default function Dashboard() {
 
   return (
     <div className="container md:overflow-hidden h-screen max-w-screen-2xl md:flex">
+      <DeleteMe open={isOpenCredit} onClose={() => setIsOpenCredit(false)}>
+        <div className="credits p-4 bg-gray-100 dark:bg-[#333333] rounded-xl">
+          <h3 className="font-bold dark:text-gray-300 text-base sm:text-lg text-center pb-1 sm:pb-4"><span className="text-purple-500">CICT</span> Voting System</h3>
+          <div className="devs text-gray-800 dark:text-gray-300 font-semibold">
+            <div className="una flex flex-col text-sm sm:text-base sm:flex-row">
+              <h4 className="flex-1 break-words font-normal">Developers:</h4>
+              <h4 className="flex-1 break-words pl-3 sm:pl-0">Menard M. Pajares</h4>
+            </div>
+            <div className="dalawa flex flex-col text-sm sm:text-base sm:flex-row border-b dark:border-zinc-700">
+              <h4 className="flex-1 break-words"></h4>
+              <h4 className="flex-1 break-words pl-3 sm:pl-0">Liza Mae N. Necerio</h4>
+            </div>
+            <div className="tatlo flex flex-col text-sm sm:text-base sm:flex-row pt-2">
+              <h4 className="flex-1 break-words font-normal">Recipient:</h4>
+              <h4 className="flex-1 break-words pl-3 sm:pl-0"><span className="text-purple-500">C</span>ollege of <span className="text-purple-500">I</span>nformation and <span className="text-purple-500">C</span>ommunication <span className="text-purple-500">T</span>echnology <span className="text-red-500">T</span>aguig <span className="text-red-500">C</span>ity <span className="text-red-500">U</span>niversity</h4>
+            </div>
+          </div>
+          <h3 className="text-right pt-4 opacity-60 text-black dark:text-white text-sm sm:text-base">Version 1.0</h3>
+        </div>
+      </DeleteMe>
       <nav
         className={` sidebar overflow-hidden z-20 fixed md:static w-4/5 h-screen rounded-r-3xl md:rounded-r-lg bg-[#444A5E] dark:bg-[#303030] text-white shadow-2xl ${
           isSidebarOpen ? 'translate-x-0 flex-[6]' : '-translate-x-full'
@@ -61,6 +86,18 @@ export default function Dashboard() {
               >
                 {isSidebarOpen && (<RxCross2 size={35} className="text-[#7268EF] shadow-sm rounded-lg bg-gray-600 dark:bg-zinc-700"/>)}
               </button>
+            </div>
+
+            <div className="hero flex justify-end gap-5 items-center py-1 px-3 rounded-full">
+              <span className="cursor-pointer" onClick={switchMode}>
+                { isNight ? ( <BsFillSunFill className='text-gray-400 hover:text-gray-200' size={18}/>) : ( < BsMoonFill className='text-[#eaefff] hover:text-slate-300' size={18} /> )}
+              </span>
+
+              <span onClick={() => setIsOpenCredit(true)} className='flex cursor-pointer'>
+                <span style={{ display: 'inline-block' }}>
+                  <CgInfo className="text-[#eaefff] dark:text-gray-400 dark:hover:text-gray-300" size={23} />
+                </span>
+              </span>
             </div>
 
             <ul className="px-3 flex flex-col gap-4 pt-10 pop-medium tracking-wider">
